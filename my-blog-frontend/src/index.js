d1bc3b8cc7b76899bc52b59c6db8972464789a90
jsBear92 <js104427@gmail.com>
2023-02-13 13:05:37 +0800

    add Firebase Auth to React

--- my-blog-frontend/src/index.js
+++ my-blog-frontend/src/index.js
@@ -3,6 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: process.env.MY_BLOG_apiKey,
+  authDomain: process.env.MY_BLOG_authDomain,
+  projectId: process.env.MY_BLOG_projectId,
+  storageBucket: process.env.MY_BLOG_storageBucket,
+  messagingSenderId: process.env.MY_BLOG_messagingSenderId,
+  appId: process.env.MY_BLOG_appId
+};
+
+const app = initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
